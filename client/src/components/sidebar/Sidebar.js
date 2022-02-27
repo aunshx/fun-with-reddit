af58320a6a4bc72842c7207031dfb656b5d16c85
aunshx <aunsh.sb@gmail.com>
2022-02-27 22:43:17 +0530

    alternating the sidebar

--- client/src/components/sidebar/Sidebar.js
+++ client/src/components/sidebar/Sidebar.js
@@ -4,33 +4,66 @@ import PropTypes from 'prop-types'
 const Sidebar = ({ typeSubreddit }) => {
     const [subRedditNumber, setSubRedditNumber] = useState(0)
 
-    useEffect(() => {
-        switch(true){
-            case typeSubreddit === 'ProgrammerHumor':
-                setSubRedditNumber(0);
-                break
-            case typeSubreddit === 'AskReddit':
-                setSubRedditNumber(1);
-                break
-            case typeSubreddit === 'funny':
-                setSubRedditNumber(2);
-                break
-            case typeSubreddit === 'gamming':
-                setSubRedditNumber(3);
-                break
+    // useEffect(() => {
+    //     switch(true){
+    //         case typeSubreddit === 'ProgrammerHumor':
+    //             setSubRedditNumber(0);
+    //             break
+    //         case typeSubreddit === 'AskReddit':
+    //             setSubRedditNumber(1);
+    //             break
+    //         case typeSubreddit === 'funny':
+    //             setSubRedditNumber(2);
+    //             break
+    //         case typeSubreddit === 'gaming':
+    //             setSubRedditNumber(3);
+    //             break
 
-            default:
-                return null
+    //         default:
+    //             return null
+    //     }
+    // }, [])
+
+    const selectSubreddit = (type) => {
+        if(type === 'programmerHumor'){
+                            setSubRedditNumber(0);
         }
-    }, [])
+        if (type === "askReddit") {
+                            setSubRedditNumber(1);
+        }
+        if (type === "funny") {
+                            setSubRedditNumber(2);
+        }
+        if (type === "gaming") {
+             setSubRedditNumber(3);
+        }
+    }
   return (
     <div className='sidebar'>
-      <div className={subRedditNumber === 0 ? "sub-name-active" : "sub-name"}>
+      <div
+        className={subRedditNumber === 0 ? "sub-name-active" : "sub-name"}
+        onClick={() => selectSubreddit("programmerHumor")}
+      >
         r/ProgrammerHumor
       </div>
-      <div className={subRedditNumber === 1 ? "sub-name-active" : "sub-name"}>r/AskReddit</div>
-      <div className={subRedditNumber === 2 ? "sub-name-active" : "sub-name"}>r/funny</div>
-      <div className={subRedditNumber === 3 ? "sub-name-active" : "sub-name"}>r/gaming</div>
+      <div
+        className={subRedditNumber === 1 ? "sub-name-active" : "sub-name"}
+        onClick={() => selectSubreddit("askReddit")}
+      >
+        r/AskReddit
+      </div>
+      <div
+        className={subRedditNumber === 2 ? "sub-name-active" : "sub-name"}
+        onClick={() => selectSubreddit("funny")}
+      >
+        r/funny
+      </div>
+      <div
+        className={subRedditNumber === 3 ? "sub-name-active" : "sub-name"}
+        onClick={() => selectSubreddit("gaming")}
+      >
+        r/gaming
+      </div>
     </div>
   );
 };
