ceb2bbc6d773279624c6b22eacf0587f49a56761
aunshx <aunsh.sb@gmail.com>
2022-02-28 15:06:11 +0530

    aunsh.com footer

--- client/src/components/sidebar/Sidebar.js
+++ client/src/components/sidebar/Sidebar.js
@@ -7,28 +7,34 @@ const Sidebar = ({ type, changeType }) => {
   return (
     <div className='sidebar'>
       <div
-        className={type === 'programmer-humor' ? "sub-name-active" : "sub-name"}
+        className={type === "programmer-humor" ? "sub-name-active" : "sub-name"}
         onClick={() => changeType("programmer-humor")}
       >
         r/ProgrammerHumor
       </div>
       <div
-        className={type === 'memes' ? "sub-name-active" : "sub-name"}
+        className={type === "memes" ? "sub-name-active" : "sub-name"}
         onClick={() => changeType("memes")}
       >
         r/memes
       </div>
       <div
-        className={type === 'thats-insane' ? "sub-name-active" : "sub-name"}
+        className={type === "thats-insane" ? "sub-name-active" : "sub-name"}
         onClick={() => changeType("thats-insane")}
       >
         r/ThatsInsane
       </div>
       <div
-        className={type === 'gaming' ? "sub-name-active" : "sub-name"}
+        className={type === "gaming" ? "sub-name-active" : "sub-name"}
         onClick={() => changeType("gaming")}
       >
         r/gaming
+      </div>
+      <div className='sub-name-creator'>
+        <a href='https://aunsh.com' target={"_blank"} rel='noreferrer nofollow'>
+          <span className='copyright'>{String.fromCodePoint("0X00A9")}</span>{" "}
+          aunsh 2022
+        </a>
       </div>
     </div>
   );
